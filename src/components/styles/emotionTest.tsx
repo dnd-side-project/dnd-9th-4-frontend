84f36f1b341bab9c2fce2b0a5d0941f29e2d7165
easyhyun00 <leejihyun0324@gmail.com>
2023-07-26 18:24:50 +0900

    :seedling: Init: 프로젝트 초기 세팅

---
+++ src/components/styles/emotionTest.tsx
@@ -0,0 +1,5 @@
+import { css } from '@emotion/react'
+
+export const EmotionTest = css({ 
+    backgroundColor: 'yellow'
+})
---
+++ src/pages/loginPage/loginPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function LoginPage() {return (
+    <div>
+        로그인 페이지
+    </div>
+)
+}
+
+export default LoginPage
