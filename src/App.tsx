4aaff37bd09d1ed79f508f2837fbeb4c6f900e7f
Ginieee <kaj1226@naver.com>
2023-07-27 15:18:27 +0900

    🌱 Chore: ESLint & Prettier 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import MainPage from 'pages/mainPage/mainPage'
-import LoginPage from 'pages/loginPage/loginPage'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from 'pages/mainPage/mainPage';
+import LoginPage from 'pages/loginPage/loginPage';
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="login" element={<LoginPage />} />
-                </Routes>
-            </Router>
-        </div>
-    )
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
