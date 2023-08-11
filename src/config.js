c957e26a7fe0f6839e58164641a57d8b72dc4dbb
easyhyun00 <leejihyun0324@gmail.com>
2023-08-11 13:45:06 +0900

    :truck: Renamt: assets 파일 디렉토리 추가
    - 아이콘 파일 디렉토리 분류

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  restApiKey: '0239e3b799683b851fa867e85be71696',
+  redirectUri: 'http://localhost:3000/auth',
+  backendUrl: 'http://3.37.141.165:8000',
+};
+
+export default config;
