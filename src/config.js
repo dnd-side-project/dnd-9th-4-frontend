50a120dd8652c806e680bc767445a8d0b78decb4
easyhyun00 <leejihyun0324@gmail.com>
2023-08-14 20:30:53 +0900

    :rocket: Chore: 리다이렉트 URL 수정

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
   restApiKey: '0239e3b799683b851fa867e85be71696',
-  //redirectUri: 'http://localhost:3000/auth',
-  redirectUri: 'http://3.37.141.165:8000/api/v1/kakao/signup',
+  redirectUri: 'https://dnd-9th-4-newple-app.vercel.app/auth',
   backendUrl: 'http://3.37.141.165:8000',
 };
 
