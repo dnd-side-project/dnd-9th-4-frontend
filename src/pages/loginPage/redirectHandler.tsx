de5bc452dd311642c804b52d8bf8f3499c04f110
easyhyun00 <leejihyun0324@gmail.com>
2023-08-17 12:49:12 +0900

    :white_check_mark: Test: 카카오 로그인 게스트 생성 테스트

--- src/pages/loginPage/redirectHandler.tsx
+++ src/pages/loginPage/redirectHandler.tsx
@@ -2,7 +2,7 @@
 import { css } from '@emotion/react';
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import config from 'config';
+//import config from 'config';
 import { useNavigate } from 'react-router-dom';
 
 function RedirectHandler() {
@@ -14,9 +14,9 @@ function RedirectHandler() {
     console.log('인가코드', code);
     console.log(typeof code);
 
-    const codeData = {
-      code: code,
-    };
+    // const codeData = {
+    //   code: code,
+    // };
 
     // axios
     //   .post(`${config.backendUrl}/api/v1/kakao/signup`, codeData)
@@ -33,6 +33,7 @@ function RedirectHandler() {
       .then((response) => {
         // 성공적으로 응답 받았을 때 처리할 로직을 여기에 작성합니다
         console.log('응답 데이터:', response.data);
+        navigate('/onboarding');
       })
       .catch((error) => {
         // 요청이 실패하거나 에러가 발생했을 때 처리할 로직을 여기에 작성합니다
