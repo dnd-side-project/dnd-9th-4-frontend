839bcff0c303f93c134c9949e8520c7a13b7b781
easyhyun00 <leejihyun0324@gmail.com>
2023-08-22 01:59:45 +0900

    :white_check_mark: Test: 로그인 토큰 확인

---
+++ src/auth/cookie.tsx
@@ -0,0 +1,30 @@
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+/** Key, Value를 받아 Cookie 생성 후 저장
+ * @param {string} name
+ * @param {string} value
+ */
+export const setCookie = (name: string, value: string) => {
+  return cookies.set(name, value, {
+    maxAge: 604800000,
+    path: '/',
+    secure: true,
+  }); // 6시간
+};
+
+/** Key를 받아 Value를 반환
+ * @param {string} name
+ * @returns {string}
+ */
+export const getCookie = (name: string) => {
+  return cookies.get(name);
+};
+
+/** Key를 받아 쿠키를 삭제
+ * @param {string} name
+ */
+export const removeCookie = (name: string) => {
+  return cookies.remove(name);
+};
