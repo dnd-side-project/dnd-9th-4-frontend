a53115358495be4588d2ec2cd47c57d1b3768f8f
Ginieee <kaj1226@naver.com>
2023-08-24 15:36:50 +0900

    🐛 Fix: OnboardingProfileState 수정

--- src/recoil/onboardingProfileState.ts
+++ src/recoil/onboardingProfileState.ts
@@ -4,7 +4,7 @@ import { atom } from 'recoil';
 export const onboardingProfileState = atom<OnboardingProfile>({
   key: 'onboardingProfileState',
   default: {
-    // memberId: null,
+    kakaoId: null,
     userName: null,
     introduce: null,
     profileImg: null,
