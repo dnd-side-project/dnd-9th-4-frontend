36d8ac51dc015b112155c6c8b320ea56da5907b5
easyhyun00 <leejihyun0324@gmail.com>
2023-08-25 22:09:16 +0900

    :bug: Edit: 게시물 작성 null 수정

--- src/recoil/postEdit.ts
+++ src/recoil/postEdit.ts
@@ -11,32 +11,16 @@ interface postEditData {
   runtime: string | null | undefined;
 }
 
-/*
-    {
-        "sport":"HIKING",
-        "tags": [
-            "운동",
-            "20대"
-        ],
-        "title": "한라산 올라가실 분~",
-        "content": "취미로 등산하는데 한라산 꼭 가보고 싶었어요!!",
-        "region" : "대전광역시 유성구",
-        "gender": "여성",
-        "age": "20대 초반",
-        "runtime":"2023-09-12 13:00:00"
-    }
-*/
-
 export const postEditState = atom<postEditData>({
   key: 'postEditState',
   default: {
-    sport: null,
+    sport: '',
     tags: [],
-    title: null,
-    content: null,
-    region: null,
-    gender: null,
-    age: null,
-    runtime: null,
+    title: '',
+    content: '',
+    region: '',
+    gender: '',
+    age: '',
+    runtime: '',
   },
 });
